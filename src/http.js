1df2535c46c0d3b316406d29daa38e3fc0a373aa
Brian Helba <brian.helba@kitware.com>
2018-07-30 12:59:36 -0400

    Allow getting API token from cookies

--- src/http.js
+++ src/http.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import * as Cookies from 'js-cookie';
 
 const covalicApiBase = 'https://challenge.kitware.com/api/v1/';
 
@@ -6,7 +7,10 @@ const http = axios.create({
   baseURL: covalicApiBase,
 });
 
-const token = window.location.hash.replace(/^#/, '');
+const token =
+  window.location.hash.replace(/^#/, '') ||
+  Cookies.get('girderToken') ||
+  null;
 if (token) {
   http.defaults.headers.common['Girder-Token'] = token;
 }
