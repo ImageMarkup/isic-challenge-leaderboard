0f66cc1b03df9130a3a760245b6e707fa10e8a1c
Brian Helba <brian.helba@kitware.com>
2018-07-30 16:30:44 -0400

    Abstract review mode

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -8,10 +8,14 @@
   >
     <template slot="items" slot-scope="{ item: submission, expanded }">
       <td>
-        {{ submission.rank }}
+        {{ submission.number }}
+        <br>
+        <code>{{ submission._id }}</code>
       </td>
       <td>
-        <a :href="submission.organizationUrl">{{ submission.organization }}</a>
+        <a :href="submission.organizationUrl" target="_blank" rel="noopener noreferrer">
+          {{ submission.organization }}
+        </a>
         <br>
         <i>({{ submission.creatorName }})</i>
         <v-alert
@@ -22,26 +26,43 @@
           Team URL: <code>{{ submission.organizationUrl }}</code>
         </v-alert>
       </td>
+
+      <td>
+        <v-edit-dialog
+          lazy
+          large
+          persistent
+          :return-value.sync="submission.meta.authors"
+          @save="saveSubmissionMeta(submission)"
+        >
+          <template v-if="submission.meta.authors">
+            <i>{{ submission.meta.authors }}</i>
+          </template>
+          <template v-else>
+            <i>Click to set</i>
+          </template>
+          <v-textarea
+            slot="input"
+            v-model="submission.meta.authors"
+            label="Edit Authors"
+            autofocus
+          >
+          </v-textarea>
+        </v-edit-dialog>
+      </td>
+
       <td>
         {{ submission.approach }}
       </td>
       <td>
         <template v-if="submission.documentationUrl">
-          <a :href="submission.documentationUrl">
+          <a :href="submission.documentationUrl" target="_blank" rel="noopener">
             <v-icon color="green">assignment</v-icon>
           </a>
         </template>
         <template v-else>
           <v-icon color="red">close</v-icon>
         </template>
-        <v-alert
-          v-if="admin"
-          :value="true"
-          color="info"
-        >
-          <code>{{ submission.documentationUrl }}</code>
-          <v-icon @click.stop="clearDocumentation(submission._id)" color="red">delete</v-icon>
-        </v-alert>
       </td>
       <td>
         <template v-if="submission.meta.usesExternalData">
@@ -57,12 +78,41 @@
         <v-icon v-if="submission.meta.agreeToSharingPolicy" color="green">check</v-icon>
         <v-icon v-else color="red">close</v-icon>
       </td>
-      <td>
-        <v-tooltip bottom>
-          <span slot="activator">{{ submission.overallScore.toPrecision(3) }}</span>
-          <span>{{ submission.overallScore }}</span>
-        </v-tooltip>
 
+      <td>
+        <template v-if="submission.meta.documentationReview">
+          <template v-if="submission.meta.documentationReview.accepted">
+            <v-icon color="green">check</v-icon>
+            Accepted <i>({{ submission.meta.documentationReview.userLogin }})</i>
+          </template>
+          <template v-else>
+            <v-icon color="red">close</v-icon>
+            Rejected <i>({{ submission.meta.documentationReview.userLogin }})</i>
+          </template>
+          <v-btn
+            @click.stop="reviewDocumentation(submission, null)"
+            small
+            color="orange"
+          >
+            Reset
+          </v-btn>
+        </template>
+        <template v-else>
+          <v-btn
+            @click.stop="reviewDocumentation(submission, true)"
+            small
+            color="green"
+          >
+            Accept
+          </v-btn>
+          <v-btn
+            @click.stop="reviewDocumentation(submission, false)"
+            small
+            color="red"
+          >
+            Reject
+          </v-btn>
+        </template>
       </td>
     </template>
     <template slot="footer">
@@ -80,6 +130,7 @@ export default {
 
   props: {
     phaseId: String,
+    user: Object,
   },
 
   data() {
@@ -102,12 +153,16 @@ export default {
     headers() {
       return [
         {
-          text: 'Rank',
-          value: 'rank',
+          text: 'Number (ID)',
+          value: 'number',
         },
         {
-          text: 'Team',
+          text: 'Team (Submitter User)',
           value: 'organization',
+        },
+        {
+          text: 'Author Names',
+          value: null,
         },
         {
           text: 'Approach Name',
@@ -132,22 +187,42 @@ export default {
             : []
         ),
         {
-          text: 'Final Score (Balanced Multi-class Accuracy)',
-          value: 'overallScore',
+          text: 'Manuscript Reviewed',
+          value: 'meta.documentationReview',
+          sortable: false,
         },
       ];
     },
   },
 
   methods: {
-    async clearDocumentation(submissionId) {
+    async reviewDocumentation(submission, accepted) {
+      if (accepted === true || accepted === false) {
+        this.$set(
+          submission.meta,
+          'documentationReview',
+          {
+            accepted,
+            time: new Date().toISOString(),
+            userId: this.user._id, // eslint-disable-line no-underscore-dangle
+            userLogin: this.user.login,
+          },
+        );
+      } else {
+        this.$delete(submission.meta, 'documentationReview');
+      }
+
+      await this.saveSubmissionMeta(submission);
+    },
+
+    async saveSubmissionMeta(submission) {
       // URLSearchParams causes a 'application/x-www-form-urlencoded' body
       const requestBody = new URLSearchParams();
-      requestBody.append('documentationUrl', '');
+      requestBody.append('meta', JSON.stringify(submission.meta));
 
       await http.request({
         method: 'put',
-        url: `covalic_submission/${submissionId}`,
+        url: `covalic_submission/${submission._id}`,
         data: requestBody,
         withCredentials: false,
         responseType: 'json',
@@ -166,15 +241,15 @@ export default {
           params: {
             limit: 0,
             offset: 0,
-            sort: 'overallScore',
-            sortdir: -1,
+            sort: 'organization',
+            sortdir: 1,
             phaseId: this.phaseId,
           },
           withCredentials: false,
           responseType: 'json',
         });
         submissionsResponse.data.forEach((submission, index) => {
-          submission.rank = index + 1; // eslint-disable-line no-param-reassign
+          submission.number = index + 1; // eslint-disable-line no-param-reassign
         });
 
         this.submissions = submissionsResponse.data;
@@ -183,6 +258,9 @@ export default {
       }
 
       this.loading = false;
+
+      // Polling causes edit boxes to refresh while typing
+      // window.setTimeout(this.loadData.bind(this), 5000);
     },
   },
 };
