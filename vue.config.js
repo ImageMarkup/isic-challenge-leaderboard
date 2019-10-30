7eb9c99e1ee822d8c6f8f9e6f1c08adefbc8c9e1
Brian Helba <brian.helba@kitware.com>
2019-10-30 10:23:57 -0400

    Remove dev proxy

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,3 @@
 module.exports = {
   lintOnSave: false,
-
-
-   devServer: {
-     port: 8081,
-
-     proxy: {
-       '/api': {
-         target: 'http://localhost:8000',
-         changeOrigin: true,
-       }
-     }
-   }
-}
+};
